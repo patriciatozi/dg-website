b98f235f1a0284e5ff1db0c7b3642345bb1a3a6c
patriciatozi <ptrc.tozi@hotmail.com>
2021-04-10 13:51:52 -0300

    Adding models for tags and social media objects

--- myapp/core/models.py
+++ myapp/core/models.py
@@ -8,6 +8,11 @@ STATUS = (
 )
 
 class Post(models.Model):
+
+    """
+    Model representing every post published by Data Girls members
+    """
+
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
@@ -15,6 +20,9 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    tag = models.ManyToManyField('Tag')
+
+    image = models.ImageField(default='src/img/default.png')
 
     class Meta:
         ordering = ['-created_on']
@@ -22,3 +30,29 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+
+    """
+    Model representing the tag for each post
+    """
+
+    word = models.CharField(max_length=35)
+    slug = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=False)
+
+    def __unicode__(self):
+        return self.word
+
+
+class SocialMedia(models.Model):
+
+    """
+    Model created for each social media instance
+    """
+
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200, unique=True)
+    url = models.URLField(max_length=200)
+    image = models.ImageField(default='static/img/default.png')
