5bda13f60fc5e2b2dccee1ff169cf28795612257
patriciatozi <ptrc.tozi@hotmail.com>
2021-05-23 00:23:42 -0300

    Folders adjusments

--- myapp/core/views.py
+++ myapp/core/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import SocialMedia
 
-# Create your views here.
+def index(request):
+    social_medias = SocialMedia.objects.all()
+    return render(request, 'index.html', {})
--- myapp/myapp/urls.py
+++ myapp/myapp/urls.py
@@ -15,13 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import index
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
-]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('core/', include('core.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
