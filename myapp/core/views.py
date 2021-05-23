fa9d014e2f0ecf6e6d5b174f91c6548f98761eee
patriciatozi <ptrc.tozi@hotmail.com>
2021-05-23 17:50:06 -0300

    Added social media info inside homepage

--- myapp/core/views.py
+++ myapp/core/views.py
@@ -3,4 +3,4 @@ from .models import SocialMedia
 
 def index(request):
     social_medias = SocialMedia.objects.all()
-    return render(request, 'index.html', {})
+    return render(request, 'index.html', {'social_medias':social_medias})
